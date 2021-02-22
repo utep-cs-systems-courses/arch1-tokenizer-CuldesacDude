224e16da346ee32e44e83602295dd45ac76fb143
Kevin Ramirez <krramirez@miners.utep.edu>
2021-02-21 23:49:21 -0700

    did a little of clean up and initilized the makefile

---
+++ src/mainFuncs.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+#include "tokenNew.c"
+
+int main (){
+
+  //SPACE_CHAR: first
+  printf("\n>Space_Char: %d",space_char('f')); // 0
+  printf("\n>Space_Char: %d", space_char(' ')); // 1
+
+  //NON-SPACE_CHAR; TEST 'wrong?'
+  printf("\n>Non_Space_Char: %d", non_space_char('g'));
+  printf("\n>Non_Space_Char: %d\n",non_space_char(' '));
+
+  //WORD_START
+  char str1[] = "   Hello";
+  char *ptr = word_start(str1);
+
+  printf("1: %p\n",ptr);
+  printf("2: %p\n",&str1[3]);
+  printf("LETTER: %c\n", str1[3]);
+
+  //WORD_TERMINATOR
+
+  char str2[] = "Hello ";
+  char *ptr2 = word_terminator(str2);
+
+  printf("1: %p\n",ptr2);
+  printf("2: %p\n",&str2[5]);
+  printf("LETTER: %c\n",str2[5]);
+
+  //WORD_COUNTER
+  printf("\nWord_counter: %i\n",count_words(" Hello world "));
+
+  return 0;
+
+}
--- src/tokenNew.c
+++ src/tokenNew.c
@@ -2,38 +2,6 @@
 #include <stdlib.h>
 #include "tokenizer.h"
 
-int main (){ 
-    
-  //SPACE_CHAR: first
-  printf("\n>Space_Char: %d",space_char('f')); // 0
-  printf("\n>Space_Char: %d", space_char(' ')); // 1
-
-  //NON-SPACE_CHAR; TEST 'wrong?'
-  printf("\n>Non_Space_Char: %d", non_space_char('g'));
-  printf("\n>Non_Space_Char: %d\n",non_space_char(' '));
-
-  //WORD_START
-  char str1[] = "   Hello";
-  char *ptr = word_start(str1);
-  
-  printf("1: %p\n",ptr);
-  printf("2: %p\n",&str1[3]);
-  printf("LETTER: %c\n", str1[3]);
-
-  //WORD_TERMINATOR
-  char str2[] = "Hello ";
-  char *ptr2 = word_terminator(str2);
-
-  printf("1: %p\n",ptr2);
-  printf("2: %p\n",&str2[5]);
-  printf("LETTER: %c\n",str2[5]);
-  
-  //WORD_COUNTER
-  printf("\n%i\n",count_words(" Hello world "));
-  
-  return 0;
-}
-
 int space_char(char c){
   if(c=='\t'||c==' '||c=='\n'){
     return 1;
